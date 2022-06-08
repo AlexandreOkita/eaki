4515a3b2192340e28f019b1b61815c1f2195b534
alexandre.okita <alexandre.okita@ifood.com.br>
2022-06-08 01:12:05 -0300

    configure text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:eaki/components/option_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const EakiMainWidget());
+  runApp(const ProviderScope(child: EakiMainWidget()));
 }
 
 class EakiMainWidget extends StatelessWidget {
@@ -12,8 +14,16 @@ class EakiMainWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Eaki',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primaryColor: const Color(0xff0E4DA4),
+          fontFamily: "Inter",
+          textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            bodyText1: TextStyle(
+              fontSize: 16,
+              color: Color(0xff263238),
+              fontWeight: FontWeight.w400,
+            ),
+          )),
       home: const HomePage(),
     );
   }
@@ -24,9 +34,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screen = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: const Text("Eaki")),
-      body: const Center(child: Text("Home")),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: screen.width * 0.1),
+        child: Center(
+          child: OptionButton(
+            text: "Consulta Agendada",
+            onPressed: () => print("aoba"),
+          ),
+        ),
+      ),
     );
   }
 }
